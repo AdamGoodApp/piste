e9bd46402a88c09aa22fecbd0479031bb0be6674
Adam <ar@getautomata.com>
2017-06-08 18:46:23 +0100

    Add basic searching in OffPiste

--- app/models/off_piste.rb
+++ app/models/off_piste.rb
@@ -1,3 +1,10 @@
 class OffPiste < ApplicationRecord
   searchkick
+
+  # Search field via elastic search
+  # elastic search includes partial searching
+  def self.elastic_search(field)
+    OffPiste.search(field)
+  end
+
 end
--- spec/models/off_piste_spec.rb
+++ spec/models/off_piste_spec.rb
@@ -2,8 +2,16 @@ describe OffPiste do
 
   let(:piste) { FactoryGirl.create :piste }
 
-  context '::search' do
+  before(:each) do
+    OffPiste.reindex
+  end
 
+  context '::search' do
+    it { expect(OffPiste).to respond_to :elastic_search }
+
+    it "should allow searching of an off-piste line by name" do
+      expect(OffPiste.elastic_search("Crochues-Berard Traverse").first.piste_id).to eq(piste.piste_id)
+    end
   end
 
 end
