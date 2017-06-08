ee6b04e67ef4cd116e8e69132c05a01c89aefc55
Adam <ar@getautomata.com>
2017-06-08 20:51:21 +0100

    Fix gt and lt switch

--- app/models/off_piste.rb
+++ app/models/off_piste.rb
@@ -12,18 +12,18 @@ class OffPiste < ApplicationRecord
       search_by_greater(params) if params[:filter] == "gt"
       search_by_less(params) if params[:filter] == "lt"
     else
-      OffPiste.search('*', order: { params[:field] => params[:order] }).map { |p| p }
+      OffPiste.search('*', order: { params[:field] => params[:order] }).map { |w| w }
     end
   end
 
   private
 
   def self.search_by_greater(params)
-    OffPiste.search('*', where: { params[:field] => {gt: params[:field_number].to_i} } , order: { params[:field] => params[:order] }).map { |p| p }
+    OffPiste.search('*', where: { params[:field] => {gt: params[:field_number].to_i} } , order: { params[:field] => params[:order] }).map { |w| w }
   end
 
   def self.search_by_less(params)
-    OffPiste.search('*', where: { params[:field] => {lt: params[:field_number].to_i} } , order: { params[:field] => params[:order] }).map { |p| p }
+    OffPiste.search('*', where: { params[:field] => {lt: params[:field_number].to_i} } , order: { params[:field] => params[:order] }).map { |w| w }
   end
 
 end
