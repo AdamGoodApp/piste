fa9cb0468c6da9d148200519776cc8fba98cf85e
Adam <ar@getautomata.com>
2017-06-08 18:21:13 +0100

    Add OffPiste Factory

---
+++ spec/factories/off_pistes.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :piste, class: OffPiste do
+    piste_id 9514
+    name "Crochues-Berard Traverse"
+    description "From the top of the Floria Drag Lift, ski north for 20 metres and put skins on and skin northwards towards the Col Crochues. After around 250 metres of height gain it is usually necessary to take skins off and bootpack the remaining 50 vertical metres up to the Col. Crampons are generally not required for the bootpack but may be useful in icy conditions. From the Col, make a long descending traverse rightwards, passing under the Aiguilles Crochues west face and the Pointe Alphonse Favre south face to reach a large shoulder at c.2300 metres at the foot of the Pointe Alphonse Favre's west ridge. The latter part of this traverse is exposed so take care and keep concentrating all the way! Once at the shoulder, put skins back on and climb north-east up to the Col de Berard. From here, descend the wonderful Berard valley to the tree line. There are countless variations which can be taken to reach the tree line. Once there, stay right of the river and ski to the hamlet of Le Buet via a good track and the occasional tricky section of tight tree skiing. From Le Buet, take the train back to Les Praz and the Flégère carpark."
+    ski_difficulty 2
+    geo_data {}
+  end
+
+end
---
+++ spec/models/off_piste_spec.rb
@@ -0,0 +1,9 @@
+describe OffPiste do
+
+  let(:piste) { FactoryGirl.create :piste }
+
+  context '::search' do
+
+  end
+
+end
